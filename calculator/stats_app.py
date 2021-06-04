44008fdfe48da7188f2341f7c73d0ef1466337c0
csklu <csklu@cesiumsolutions.com>
2021-06-04 03:56:26 +0200

    Lesson 1: Organize into subdirectory modules.

--- calculator/stats_app.py
+++ calculator/stats_app.py
@@ -1,29 +1,29 @@
-import stats
-from stats_menu import *
+from cesium.stats import math
+from cesium.stats import menu
 
 def main():
 
-    printIntro()
+    menu.printIntro()
     
     values = None
     quit = False
 
     while not quit:
-        printMenu()
+        menu.printMenu()
         choice = int(input())
 
         if choice == 1:
-            values = getValues()
+            values = menu.getValues()
         elif choice == 2:
-            showValues(values)
+            menu.showValues(values)
         elif choice == 3:
-            print('Average is: ', stats.average(values))
+            print('Average is: ', math.average(values))
         elif choice == 0:
             quit = True
         else:
             print('Unrecognized input, try again please')
 
-    printOutro()
+    menu.printOutro()
 
 if __name__ == "__main__":
     main()
